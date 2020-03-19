9b952c0c0c50fa087ea93affeed162185c768218
MohamedIbrahim <mohamed.ibrahim15@fci.helwan.edu.eg>
2020-03-19 06:20:56 +0200

    start-project

---
+++ app/src/main/java/com/mi/moviesapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mi.moviesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class MainActivity : AppCompatActivity() {
+
+
+}
